eefd36a556c936dfd4e37f9b9f44536934077b65
QuocTrieu <phamnhuquoctrieu307@gmail.com>
2026-04-12 14:46:12 +0700

    modify: (be) comment with jwt

--- backend/src/routes/comments.ts
+++ backend/src/routes/comments.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { createComment, getComments, deleteComment, updateComment } from "../controllers/commentContrller.js";
+import { verifyJWTRequired } from "../middleware/auth.js";
 
 const router = express.Router();
-router.post("/", createComment);
+
+router.post("/", verifyJWTRequired, createComment);
 router.get("/:campaignId", getComments);
-router.delete("/:commentId", deleteComment);
-router.put("/:commentId", updateComment);
+router.delete("/:commentId", verifyJWTRequired, deleteComment);
+router.put("/:commentId", verifyJWTRequired, updateComment);
 
 export default router;
